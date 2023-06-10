ee6163e9fc19897078106567bbcc7cd8aaaebb47
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-06-10 23:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
+const express = require('express');
+const path = require('path');
 
- 'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+const app = express();
 
-function rot13(encodedStr) {
-  var words = encodedStr.split(" "); // String to Array
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-  for (let i = 0; i < words.length; i++) { // itrat all the word & going decoded
-    const word = words[i];// for giving word string
-    let decoded_word = "";
-    for (let j = 0; j < word.length; j++) {
-      var char = word.charAt(j);
-      var decoded_char = lookup[char];
-      decoded_word += decoded_char;
-    }
-    decodedArr.push(decoded_word);
-  }
-  return decodedArr.join(" ");// join my all decoded word in my single data
-}
+const answer = require("./index.js");
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+app.listen(3000, () => {
+  console.log('server started');
+});
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-module.exports = rot13;
+app.get('/', (req, res) => {
+  res.send(answer("SERR YBIR? NPPVBWBO"))
+});
