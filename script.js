a9842cd45a39d42f13859dd313bb26e1c886cb05
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-06-11 00:07:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,29 @@
-
-  
-
- const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
+const lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+function rot13(encodedStr){
+   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-	for(let i=0;i<str.length;i++){
-		if(lookup[str[i]]==undefined){
-			decodedArr+=str[i]
-		}
-		else{
-			decodedArr+=lookup[str[i]]
-		}
-		return decodedArr;
-	}
-
-  return; //return decodedArr
+	for(let i = 0 ; i < encodedStr.length ; i++){
+        if(lookup[encodedStr[i]] === undefined){
+            decodedArr = decodedArr + encodedStr[i];
+        }
+        else{
+            decodedArr = decodedArr + lookup[encodedStr[i]];
+        }
+    }
+  return decodedArr ;//return decodedArr
 }
 
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-window.rot13 = rot13;
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
+
+module.exports = rot13;
