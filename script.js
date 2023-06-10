ec750da91d8c57878a99469afcac030f818faf0f
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-06-10 23:22:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const path = require('path');
+const lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
+};
 
-const app = express();
+function rot13(encodedStr) {
+  let decodedStr = '';
 
-const answer = require("./index.js");
+  for (let i = 0; i < encodedStr.length; i++) {
+    let asciiCode = encodedStr.charCodeAt(i);
 
-app.listen(3000, () => {
-  console.log('server started');
-});
+    if (asciiCode < 91 && asciiCode > 64) {
+      asciiCode = ((asciiCode - 65 + 13) % 26) + 65;
+      const character = String.fromCharCode(asciiCode);
+      decodedStr = decodedStr + character;
+    } else {
+      const character2 = encodedStr.charAt(i);
+      decodedStr = decodedStr + character2;
+    }
+  }
 
-app.get('/', (req, res) => {
-  res.send(answer("SERR YBIR? NPPVBWBO"))
-});
+  return decodedStr;
+}
+
+module.exports = rot13;
